39d578f0d2362fc8dc07a91932e93ee945f60489
lisa <lisaauta018@gmail.com>
2024-10-11 22:33:32 +0300

    C1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return messages;
+}
+//console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    };
+};
+//console.log(countDown(27))
